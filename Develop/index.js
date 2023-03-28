156da05ae7f5047bf2fa84295e022f207997c204
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-03-28 18:24:56 -0400

    added questions and created file

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,23 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    "What was your motivation?", 
+    "Why did you build this project?", 
+    "What problem does it solve?", 
+    "What did you learn?"
+];
+console.log(questions)
+
+// Create README.md file 
+const fs = require('fs');
+fs.appendFile('README.txt', `${process.argv[2]}\n`, (err) =>
+err ? console.error(err) : console.log('Commit logged!')
+);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+// pull the questions array and add a place for input
 
 // TODO: Create a function to initialize app
 function init() {}
