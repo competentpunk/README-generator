ef3cfa32f3d6a50b7027a38dd7a83f764e958841
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-03-28 22:07:56 -0400

    Added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,49 @@
-
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
-const path = require('path');
-// Always put required uptop
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [ 
-    {
-        type: 'input', 
-        name: 'motivation', 
-        message: 'What was your motivation?',
-}
-    // "Why did you build this project?", 
-    // "What problem does it solve?", 
-    // "What did you learn?"
+const questions = [
+  {
+    type: "input",
+    name: "motivation",
+    message: "What was your motivation?",
+  },
+  {
+    type: "input",
+    name: "why",
+    message: "Why did you build this project?",
+  },
+  {
+    type: "input",
+    name: "problemsolve",
+    message: "What problem does it solve?",
+  },
+  {
+    type: "input",
+    name: "learn",
+    message: "What did you learn?",
+  },
 ];
-console.log(questions)
 
-// Create README.md file 
-
+// Create README.md file
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
-    // us fs here to write file and path
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  // us fs here to write file and path
 }
 // pull the questions array and add a place for input
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((inquirerResponses) => {
-        console.log('Creating README.md')
-        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }))
-    });
-    // inquirer and generateMarkdown
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("Creating README.md");
+    writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+  });
+  // inquirer and generateMarkdown
 }
 
 // Function call to initialize app
